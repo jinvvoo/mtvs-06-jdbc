77336e0b4bc8f4e439a2518ee119a5c437dab6b5
jinvvoo <jinvvookim@gmail.com>
2023-07-10 11:04:31 +0900

    완료

--- 01_lecture-source/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section03/sqlinjection/Application2.java
+++ 01_lecture-source/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section03/sqlinjection/Application2.java
@@ -1,39 +1,39 @@
-package com.ohgiraffers.section02.prepared;
+package com.ohgiraffers.section03.sqlinjection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
 
 import static com.ohgiraffers.common.JDBCTemplate.close;
 import static com.ohgiraffers.common.JDBCTemplate.getConnection;
 
 public class Application2 {
 
+    private static String empId = "200";
+    private static String empName = "' OR 1=1 AND EMP_ID = '200";
+
     public static void main(String[] args) {
 
         Connection con = getConnection();
-
         PreparedStatement pstmt = null;
         ResultSet rset = null;
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("조회하실 사번을 입력하세요 : ");
-        String empId = sc.nextLine();
-
-        String query = "SELECT EMP_ID, EMP_NAME FROM EMPLOYEE WHERE EMP_ID = ?";    //물음표를 위치홀더, 나중에 값 들어갈거다 표기
+        String query = "SELECT * FROM EMPLOYEE WHERE EMP_ID = ? AND EMP_NAME = ?";
 
         try {
             pstmt = con.prepareStatement(query);
-
-            pstmt.setString(1, empId);  //위치, 값 순서
+            pstmt.setString(1, empId);
+            pstmt.setString(2, empName);
 
             rset = pstmt.executeQuery();
 
             if (rset.next()) {
-                System.out.println(rset.getString("EMP_ID") + ", " + rset.getString("EMP_NAME"));
+                System.out.println(rset.getString("EMP_NAME") + "님 환영합니다.");
+            } else {
+                System.out.println("회원 정보가 없습니다.");
             }
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
---
+++ 01_lecture-source/chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section01/insert/MenuRepository.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.section01.insert;
+
+import com.ohgiraffers.domain.entity.Menu;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class MenuRepository {
+
+    public int insertMenu(Connection con, Menu menu) {
+
+        int result = 0;
+
+        PreparedStatement pstmt = null;
+
+
+
+
+        return result;
+    }
+
+
+}
